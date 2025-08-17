7803f33531db0029e1093ed7e2eb1b0b5002c28b
vyogalakshmi <vyogalakshmi@gmail.com>
2025-08-17 09:52:02 +0530

    Add files via upload
    Day1

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,29 @@
+package Week1.day1;
+
+public class EmployeeDetails {
+	
+  public static void main(String[] args) {
+	
+	  String Name = "John Doe";
+	  byte age = 30;
+	  double salary = 55000.75;
+	  char Gender = 'M';
+	  boolean acceptTerms=true;
+	  float rating =4.5f;
+	  long mobileNumber =9876543210L;
+	  int pincode = 56001;
+	  byte luckynumber=7;
+	  
+	  System.out.println("Name "+Name+"\n"+"Age "+age+"\n"+"Salary "+salary+"\n"+"Gender "+Gender);
+	  System.out.println("AcceptTerms "+acceptTerms+"\n"+"Rating "+rating+"\n"+"MobileNumber"+mobileNumber);
+	  System.out.println("Pincode "+pincode+"\n"+"LuckyNumber "+luckynumber);
+	  
+	  
+			  
+	  
+	  
+	  
+	  
+}
+
+}
---
+++ day1/FirstProgram.java
@@ -0,0 +1,11 @@
+package Week1.day1;
+
+public class FirstProgram {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to testleaf");
+		
+	}
+
+}
